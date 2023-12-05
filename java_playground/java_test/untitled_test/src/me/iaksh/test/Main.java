64a76bbee6e073964d14920ed0ea2d7c3be9e9c4
IAKSH <1248675212@qq.com>
2023-12-06 00:12:49 +0800

    play java

---
+++ java_playground/java_test/untitled_test/src/me/iaksh/test/Main.java
@@ -0,0 +1,9 @@
+package me.iaksh.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Stalks stalks = new Stalks();
+        System.out.println(stalks.getHostHexagram().getName());
+        System.out.println(stalks.getTransHexagram().getName());
+    }
+}
