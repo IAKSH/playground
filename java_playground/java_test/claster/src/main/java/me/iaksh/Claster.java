105aac89310b0694cfcb6b6390b4dbd37ac8bf8b
IAKSH <1248675212@qq.com>
2023-12-29 17:40:47 +0800

    多通道

--- java_playground/java_test/claster/src/main/java/me/iaksh/Claster.java
+++ java_playground/java_test/claster/src/main/java/me/iaksh/Claster.java
@@ -3,46 +3,50 @@ package me.iaksh;
 import org.lwjgl.openal.*;
 
 public class Claster {
-    private ALCCapabilities alcCapabilities;
-    private ALCapabilities alCapabilities;
-    private long device;
-    private long context;
-    private int source;
+    private int alSource;
+    private boolean playing;
 
-    private void initOpenAL() {
-        String defaultDeviceName = ALC11.alcGetString(0, ALC11.ALC_DEFAULT_DEVICE_SPECIFIER);
-        device = ALC11.alcOpenDevice(defaultDeviceName);
-
-        int[] attributes = {0};
-        context = ALC11.alcCreateContext(device, attributes);
-        ALC11.alcMakeContextCurrent(context);
-
-        alcCapabilities = ALC.createCapabilities(device);
-        alCapabilities = AL.createCapabilities(alcCapabilities);
-
-        if(!(alCapabilities.OpenAL10 && alCapabilities.OpenAL11)) {
-            System.out.println("OpenAL 10/11 is not supported in current context.");
-        }
-
-        source = AL11.alGenSources();
+    void initAlSource() {
+        alSource = AL11.alGenSources();
+        AL11.alSourcei(alSource,AL11.AL_LOOPING,1);
+        AL11.alSourcef(alSource,AL11.AL_GAIN,1.0f);
     }
 
     public Claster() {
-        initOpenAL();
+        playing = false;
+        initAlSource();
     }
 
-    public void play(Wave wave) throws InterruptedException {
-        AL11.alSourcei(source,AL11.AL_BUFFER,wave.getAlBuffer());
-        AL11.alSourcei(source,AL11.AL_LOOPING,1);
-        AL11.alSourcef(source,AL11.AL_GAIN,1.0f);
-        AL11.alSourcePlay(source);
-
-        Thread.sleep(wave.getDurationMs());
-        AL11.alSourceStop(source);
+    public void bindWave(Wave wave) {
+        stop();
+        AL11.alSourcei(alSource,AL11.AL_BUFFER,wave.getAlBuffer());
     }
 
-    public void destroy() {
-        ALC11.alcDestroyContext(context);
-        ALC11.alcCloseDevice(device);
+    public void pause() {
+        playing = false;
+        AL11.alSourcePause(alSource);
+    }
+
+    public void resume() {
+        // TODO
+        playing = true;
+    }
+
+    public void stop() {
+        playing = false;
+        AL11.alSourceStop(alSource);
+    }
+
+    public void start() {
+        playing = true;
+        AL11.alSourcePlay(alSource);
+    }
+
+    public void destroyAlSource() {
+        AL11.alDeleteSources(alSource);
+    }
+
+    public boolean isPlaying() {
+        return playing;
     }
 }
---
+++ java_playground/java_test/claster/src/main/java/me/iaksh/Mixer.java
@@ -0,0 +1,35 @@
+package me.iaksh;
+
+import org.lwjgl.openal.*;
+
+public class Mixer {
+    private ALCCapabilities alcCapabilities;
+    private ALCapabilities alCapabilities;
+    private long device;
+    private long context;
+
+    public Mixer() {
+        initOpenAL();
+    }
+
+    private void initOpenAL() {
+        String defaultDeviceName = ALC11.alcGetString(0, ALC11.ALC_DEFAULT_DEVICE_SPECIFIER);
+        device = ALC11.alcOpenDevice(defaultDeviceName);
+
+        int[] attributes = {0};
+        context = ALC11.alcCreateContext(device, attributes);
+        ALC11.alcMakeContextCurrent(context);
+
+        alcCapabilities = ALC.createCapabilities(device);
+        alCapabilities = AL.createCapabilities(alcCapabilities);
+
+        if(!(alCapabilities.OpenAL10 && alCapabilities.OpenAL11)) {
+            System.out.println("OpenAL 10/11 is not supported in current context.");
+        }
+    }
+
+    public void destoryOpenAL() {
+        ALC11.alcDestroyContext(context);
+        ALC11.alcCloseDevice(device);
+    }
+}
