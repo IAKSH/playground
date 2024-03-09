3301f647892ca365a3a7dcb69e5994af42c4271c
IAKSH <1248675212@qq.com>
2024-03-09 19:53:47 +0800

    update

--- cpp_playground/lanqiaobei/leetcode_rob_dp.cpp
+++ cpp_playground/lanqiaobei/leetcode_rob_dp.cpp
@@ -24,11 +24,37 @@ public:
         }
         return *max_element(dp.begin(),dp.end());
     }
+
+    int rob1(vector<int>& nums) noexcept {
+        // dp + 滚动数组(优化空间复杂度)
+        size_t len = nums.size();
+        if(len == 0) {
+            return 0;
+        }
+        if(len == 1) {
+            return nums[0];
+        }
+
+        int first = nums[0];
+        int second = max(nums[0],nums[1]);
+        int last;
+
+        if(len == 2) {
+            return second;
+        }
+        
+        for(size_t i = 2;i < len;i++) {
+            last = max(first + nums[i],second);
+            first = second;
+            second = last;
+        }
+        return last;
+    }
 };
 
 int main() noexcept {
-    vector<int> v{2,7,9,3,1};
+    vector<int> v{0,0};
     Solution s;
-    cout << s.rob(v) << endl;
+    cout << s.rob1(v) << endl;
     return 0;
 }
