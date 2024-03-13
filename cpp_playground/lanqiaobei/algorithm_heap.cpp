0f0a40a6de32c797d69130df2a84263e6fa2ab23
IAKSH <1248675212@qq.com>
2024-03-13 20:06:41 +0800

    update

--- cpp_playground/lanqiaobei/algorithm_heap.cpp
+++ cpp_playground/lanqiaobei/algorithm_heap.cpp
@@ -1,9 +1,9 @@
 /**
  * algorithm库中有一套完整的构建和操作最大堆的函数
  * 应该是为了实现堆排序(std::sort_heap)所设计的
- * std::push_heap (用于在添加一个元素后手动维护最大堆，通常比make_heap高效，见下面代码)
- * std::pop_heap  (用于在删除一个元素后手动维护最大堆，通常比make_heap高效)
- * std::make_heap
+ * std::push_heap (用于在添加一个元素后手动维护堆，通常比make_heap高效，见下面代码)
+ * std::pop_heap  (用于在删除一个元素后手动维护堆，通常比make_heap高效)
+ * std::make_heap 不一定创建的是大顶堆，有第三个参数，为greater<>()则生成小顶堆，上二同
  * std::sort_heap
  * std::is_heap (C++ 11)
  * std::is_heap_until (C++ 11)
