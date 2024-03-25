2c2b5958db98053ca9562da4e1f1a4f8473aad69
IAKSH <1248675212@qq.com>
2024-03-25 17:58:55 +0800

    lqb_99

---
+++ cpp_playground/lanqiaobei/lqb_99.cpp
@@ -0,0 +1,33 @@
+/**
+ * https://www.lanqiao.cn/problems/99/learning/?page=1&first_category_id=1&second_category_id=3
+ * 通过率：62.5%
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() noexcept {
+    int n,k,maxn = 0;
+    vector<pair<int,int>> chocolates;
+
+    cin >> n >> k;
+    for(int i = 0;i < n;i++) {
+        int h,w;
+        scanf("%d%d",&h,&w);
+        maxn = max(maxn,max(h,w));
+        chocolates.emplace_back(pair<int,int>(h,w));
+    }
+
+    int lsq;
+    for(lsq = maxn;lsq > 0;lsq--) {
+        int _k = 0;
+        for(const auto& cho : chocolates)
+            _k += min(cho.first,cho.second) / lsq * max(cho.first,cho.second) / lsq;
+        if(_k >= k)
+            break;
+    }
+
+    cout << lsq << '\n';
+    return 0;
+}
