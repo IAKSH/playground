bf8e131a01620b757c75b865862aff1c07824c9f
IAKSH <1248675212@qq.com>
2024-03-31 10:47:57 +0800

    lqb_2479

---
+++ cpp_playground/lanqiaobei/lqb_2479.cpp
@@ -0,0 +1,12 @@
+// https://www.lanqiao.cn/problems/2479/learning/?page=1&first_category_id=1&second_category_id=3&difficulty=30
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() noexcept {
+    int n = 0;
+    cin >> n;
+    cout << static_cast<int>(sqrt(n)) << '\n';
+    return 0;
+}
