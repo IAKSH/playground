b2cfc9815bd8c8ad9ff0d99beec2145c776e4d33
IAKSH <1248675212@qq.com>
2024-04-03 22:44:50 +0800

    update lqb_3544

--- cpp_playground/lanqiaobei/lqb_3544.cpp
+++ cpp_playground/lanqiaobei/lqb_3544.cpp
@@ -1,11 +1,11 @@
 // https://www.lanqiao.cn/problems/3544/learning/?page=1&first_category_id=1&second_category_id=3&tags=2023,%E4%BA%8C%E5%88%86&difficulty=30
 // 未经测试
+// 模拟，等下再写个区间合并+二分查找
+// 不出意外的超时了，5过5超时
 
 #include <bits/stdc++.h>
 
 using namespace std;
-
-// 模拟，等下再写个区间合并+二分查找
 
 int main() noexcept {
     int n,len;cin >> n >> len;
@@ -14,23 +14,24 @@ int main() noexcept {
     for(auto& p : v)
         cin >> p.first >> p.second;
     
-    int t = 0;
-    for(;true;t++) {
+    int cur_t = 1;
+    while(true) {
         bool flag = true;
         for(const auto& b : pipe)
             if(!b) flag = false;
         if(flag) {
-            cout << t << '\n';
-            return 0;
+            cout << cur_t - 1 << '\n';
+            break;
         }
         for(const auto& p : v) {
-            if(p.second <= t) {
-                for(int i = p.first - (t - p.second);i <= p.first + (t - p.second);i++) {
-                    if(i < 1 && i > n) continue;
-                    pipe[i - 1] = true;
+            if(p.second <= cur_t) {
+                for(int i = p.first - (cur_t - p.second);i <= p.first + (cur_t - p.second);i++) {
+                    if(i > 0 && i <= len)
+                        pipe[i - 1] = true;
                 }
             }
         }
+        ++cur_t;
     }
-    return 1;
+    return 0;
 }
