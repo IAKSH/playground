870872f11e585e081f0173ba85436d3751bccc6f
IAKSH <1248675212@qq.com>
2024-04-04 22:24:44 +0800

    lqb_3505 update (dfs only, 55%)

--- cpp_playground/lanqiaobei/lqb_3505.cpp
+++ cpp_playground/lanqiaobei/lqb_3505.cpp
@@ -12,35 +12,18 @@ using namespace std;
 static int n,m;
 static vector<float> v;
 
-#ifdef DFS
 int min_cut_cnt = INT_MAX;
 
-void dfs(float sum,int dn,int cut_cnt,int mark) noexcept {
-    if(dn == n)
-        return;
-    
-    switch(mark) {
-    case 0:
-        sum += v[dn];
-        break;
-    case 1:
-        sum += v[dn] / 2.0f;
-        ++cut_cnt;
-        break;
-    case 2:
-        break;
+void dfs(float sum,int dn,int cut_cnt) noexcept {
+    if(dn <= n && cut_cnt <= min_cut_cnt) {
+        if(sum == m)
+            min_cut_cnt = min(min_cut_cnt,cut_cnt);
+        else if(sum < m) {
+            dfs(sum,dn + 1,cut_cnt);
+            dfs(sum + v[dn] / 2.0f,dn + 1,cut_cnt + 1);
+            dfs(sum + v[dn],dn + 1,cut_cnt);
+        }
     }
-
-    if(sum > m)
-        return;
-    if(sum == m) {
-        min_cut_cnt = min(min_cut_cnt,cut_cnt);
-        return;
-    }
-
-    ++dn;
-    for(int i = 0;i < 3;i++)
-        dfs(sum,dn,cut_cnt,i);
 }
 
 int main() noexcept {
@@ -49,58 +32,12 @@ int main() noexcept {
     for(auto& i : v)
         cin >> i;
 
-    for(int i = 0;i < 3;i++)
-        dfs(0,0,0,i);
+    sort(v.begin(),v.end(),greater<float>());
+
+    dfs(0,0,0);
+    dfs(v[0] / 2.0f,0,1);
+    dfs(v[0],0,0);
 
     cout << min_cut_cnt << '\n';
     return 0;
 }
-#else
-struct Node {
-    float sum;
-    int dn;
-    int cut_cnt;
-    int mark;
-};
-
-int main() noexcept {
-    cin >> n >> m;
-    v.resize(n);
-    for(auto& i : v)
-        cin >> i;
-
-    queue<Node> nodes;
-    for(int i = 0;i < 3;i++)
-        nodes.push(Node{0,0,0,i});
-    while(!nodes.empty()) {
-        auto& node = nodes.front();
-        // check node
-        if(node.dn != n) {
-            switch(node.mark) {
-            case 0:
-                node.sum += v[node.dn];
-                break;
-            case 1:
-                node.sum += v[node.dn] / 2.0f;
-                ++node.cut_cnt;
-                break;
-            default:
-                break;
-            }
-            // if find the first one, output
-            if(node.sum == m) {
-                cout << node.cut_cnt << '\n';
-                return 0;
-            }
-            // if not, add next nodes
-            else if(node.sum < m) {
-                for(int i = 0;i < 3;i++)
-                    nodes.push(Node{node.sum,node.dn + 1,node.cut_cnt,i});
-            }
-        }
-        nodes.pop();
-    }
-    
-    return 1;
-}
-#endif
