e1f381bacae185db38d41a59620de739b3e38d51
IAKSH <1248675212@qq.com>
2024-04-11 11:28:56 +0800

    lqb_2376

---
+++ cpp_playground/lanqiaobei/lqb_2376.cpp
@@ -0,0 +1,10 @@
+// https://www.lanqiao.cn/problems/2376/learning/?page=6&first_category_id=1&second_category_id=3&sort=pass_rate&asc=0
+// 最敷衍的一集
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "ABC";
+  return 0;
+}
