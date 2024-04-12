b4f91ba68996f61d30e7e7424059a4a766a795c9
IAKSH <1248675212@qq.com>
2024-04-12 22:32:56 +0800

    cout_hex_oct_bin

---
+++ cpp_playground/lanqiaobei/cout_hex_oct_bin.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main() noexcept {
+    std::cout << std::fixed << std::setprecision(1) << 1.2330000f << '\n';
+    std::cout << 114.514f << '\n';
+    std::cout << std::oct << 114514 << '\n';
+    std::cout << std::hex << 114514 << '\n';
+    std::cout << std::bitset<16>(114514) << '\n';
+    std::cout << std::bitset<16>(-114514) << '\n';
+    std::cout << std::bitset<16>(114514U) << '\n';
+    return 0;
+}
