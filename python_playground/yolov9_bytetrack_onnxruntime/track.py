bcf2decb5d69258578f56fba208f80e33f2af479
IAKSH <1248675212@qq.com>
2024-05-05 16:10:11 +0800

    almost fixed

--- python_playground/yolov9_bytetrack_onnxruntime/track.py
+++ python_playground/yolov9_bytetrack_onnxruntime/track.py
@@ -1,39 +1,38 @@
-import argparse
 import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import argparse
 
 import cv2
 import numpy as np
-from loguru import logger
 
 import onnxruntime
 
-from utils import multiclass_nms, demo_postprocess
 from visualize import plot_tracking
 from tracker import BYTETracker
 from utils import Timer
 
-
 def make_parser():
-    parser = argparse.ArgumentParser("onnxruntime inference sample")
+    parser = argparse.ArgumentParser("onnxruntime inference")
     parser.add_argument(
         "-m",
         "--model",
         type=str,
-        default="../../bytetrack_s.onnx",
+        default="yolov9-c-converted.onnx",
         help="Input your onnx model.",
     )
     parser.add_argument(
         "-i",
         "--video_path",
         type=str,
-        default='../../videos/palace.mp4',
+        default='videos/palace.mp4',
         help="Path to your input image.",
     )
     parser.add_argument(
         "-o",
-        "--output_dir",
+        "--output_video_path",
         type=str,
-        default='demo_output',
+        default='demo_output.mp4',
         help="Path to your output directory.",
     )
     parser.add_argument(
@@ -47,13 +46,13 @@ def make_parser():
         "-n",
         "--nms_thr",
         type=float,
-        default=0.7,
+        default=0.3,
         help="NMS threshould.",
     )
     parser.add_argument(
         "--input_shape",
         type=str,
-        default="640, 640",
+        default="640,640",
         help="Specify an input shape for inference.",
     )
     parser.add_argument(
@@ -69,8 +68,7 @@ def make_parser():
     parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")
     return parser
 
-
-def preprocess(image, input_size, mean, std, swap=(2, 0, 1)):
+def preprocess(image, input_size, swap=(2, 0, 1)):
     if len(image.shape) == 3:
         padded_img = np.ones((input_size[0], input_size[1], 3)) * 114.0
     else:
@@ -86,47 +84,103 @@ def preprocess(image, input_size, mean, std, swap=(2, 0, 1)):
 
     padded_img = padded_img[:, :, ::-1]
     padded_img /= 255.0
-    if mean is not None:
-        padded_img -= mean
-    if std is not None:
-        padded_img /= std
+
     padded_img = padded_img.transpose(swap)
     padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
     return padded_img, r
 
+def nms(boxes, scores, nms_thr):
+    """Single class NMS implemented in Numpy."""
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= nms_thr)[0]
+        order = order[inds + 1]
+
+    return keep
+
+def multiclass_nms(boxes, scores, nms_thr, score_thr):
+    """Multiclass NMS implemented in Numpy"""
+    final_dets = []
+    num_classes = scores.shape[1]
+
+    for cls_ind in range(num_classes):
+        cls_scores = scores[:, cls_ind]
+        valid_score_mask = cls_scores > score_thr
+        if valid_score_mask.sum() == 0:
+            continue
+        else:
+            valid_scores = cls_scores[valid_score_mask]
+            valid_boxes = boxes[valid_score_mask]
+            keep = nms(valid_boxes, valid_scores, nms_thr)
+            if len(keep) > 0:
+                cls_inds = np.ones((len(keep), 1)) * cls_ind
+                dets = np.concatenate(
+                    [valid_boxes[keep], valid_scores[keep, None], cls_inds], 1
+                )
+                final_dets.append(dets)
+    if len(final_dets) == 0:
+        return None
+    return np.concatenate(final_dets, 0)
+
+
 class Predictor(object):
     def __init__(self, args):
-        self.rgb_means = (0.485, 0.456, 0.406)
-        self.std = (0.229, 0.224, 0.225)
         self.args = args
-        self.session = onnxruntime.InferenceSession(args.model)
+        providers = ['CUDAExecutionProvider']
+        # 'CPUExecutionProvider',
+        self.session = onnxruntime.InferenceSession(args.model, providers=providers)
         self.input_shape = tuple(map(int, args.input_shape.split(',')))
 
-    def inference(self, ori_img, timer):
+    def inference(self, ori_img,timer):
         img_info = {"id": 0}
         height, width = ori_img.shape[:2]
         img_info["height"] = height
         img_info["width"] = width
         img_info["raw_img"] = ori_img
 
-        img, ratio = preprocess(ori_img, self.input_shape, self.rgb_means, self.std)
+        img, ratio = preprocess(ori_img, self.input_shape)
         img_info["ratio"] = ratio
         ort_inputs = {self.session.get_inputs()[0].name: img[None, :, :, :]}
         timer.tic()
-        output = self.session.run(None, ort_inputs)
-        predictions = demo_postprocess(output[0], self.input_shape, p6=self.args.with_p6)[0]
+        output = self.session.run(None, ort_inputs)[0]
+
+        predictions = np.squeeze(output).T
 
         boxes = predictions[:, :4]
-        scores = predictions[:, 4:5] * predictions[:, 5:]
+        scores = predictions[:, 4:]
 
         boxes_xyxy = np.ones_like(boxes)
-        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
-        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
-        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
-        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2] / 2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3] / 2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2] / 2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3] / 2.
         boxes_xyxy /= ratio
         dets = multiclass_nms(boxes_xyxy, scores, nms_thr=self.args.nms_thr, score_thr=self.args.score_thr)
-        return dets[:, :-1], img_info
+
+        if dets is None:
+            return np.array([]), img_info
+        else:
+            return dets[:, :-1], img_info
 
 
 def imageflow_demo(predictor, args):
@@ -134,24 +188,24 @@ def imageflow_demo(predictor, args):
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
     fps = cap.get(cv2.CAP_PROP_FPS)
-    save_folder = args.output_dir
-    os.makedirs(save_folder, exist_ok=True)
-    save_path = os.path.join(save_folder, args.video_path.split("/")[-1])
-    logger.info(f"video save_path is {save_path}")
+
     vid_writer = cv2.VideoWriter(
-        save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
+        args.output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(width), int(height))
     )
     tracker = BYTETracker(args, frame_rate=30)
+
     timer = Timer()
     frame_id = 0
     results = []
+
     while True:
-        if frame_id % 20 == 0:
-            logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
+
         ret_val, frame = cap.read()
         if ret_val:
-            outputs, img_info = predictor.inference(frame, timer)
-            online_targets = tracker.update(outputs, [img_info['height'], img_info['width']], [img_info['height'], img_info['width']])
+            outputs, img_info = predictor.inference(frame,timer)
+            online_targets = []
+            if outputs.size != 0:
+                online_targets = tracker.update(outputs, [img_info['height'], img_info['width']], [img_info['height'], img_info['width']])
             online_tlwhs = []
             online_ids = []
             online_scores = []
@@ -166,7 +220,10 @@ def imageflow_demo(predictor, args):
             timer.toc()
             results.append((frame_id + 1, online_tlwhs, online_ids, online_scores))
             online_im = plot_tracking(img_info['raw_img'], online_tlwhs, online_ids, frame_id=frame_id + 1,
-                                      fps=1. / timer.average_time)
+                                      fps=1. / timer.average_time,
+                                      ids2=['good', 'broke', 'lose', 'uncovered', 'circle'], scores=online_scores)
+            cv2.imshow('Image', online_im)
+
             vid_writer.write(online_im)
             ch = cv2.waitKey(1)
             if ch == 27 or ch == ord("q") or ch == ord("Q"):
--- python_playground/yolov9_bytetrack_onnxruntime/utils/__init__.py
+++ python_playground/yolov9_bytetrack_onnxruntime/utils/__init__.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
-
-from .demo_utils import *
-from .logger import setup_logger
-from .lr_scheduler import LRScheduler
-from .setup_env import *
-from .visualize import *
 from .timer import Timer
