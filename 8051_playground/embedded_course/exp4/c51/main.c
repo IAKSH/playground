66afb8f538d872e87988c4b670c0709d34866bea
IAKSH <1248675212@qq.com>
2024-05-06 23:01:35 +0800

    8051 embedded course exp4

--- 8051_playground/embedded_course/exp4/c51/main.c
+++ 8051_playground/embedded_course/exp4/c51/main.c
@@ -1,34 +1,33 @@
 #include <reg51.h>
 
-sbit D1 = P0^4;
-char i = 0;
-char led_mod[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,
-	0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x00};
+sbit PIN_D1 = P0^4;
+unsigned char i = 0;
+unsigned char table[16] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
 
-void delay(unsigned int t) {
-	unsigned char j = 250;
-	for(;t > 0;t--)
-		for(;j > 0;j--);
+void initPorts() {
+	PIN_D1 = 1;
+	P2 = 0x00;
 }
 
-void interKey1() interrupt 0 {
-	D1 = !D1;
+void initInterrupt() {
+	EX0 = 1;
+	EX1 = 1;
+	IT0 = 1;
+	IT1 = 1;
+	EA = 1;
 }
 
-void interKey2() interrupt 2 {
-	++i;
-	i %= 16;
+void exInter0Handler() interrupt 0 {
+	PIN_D1 = !PIN_D1;
+}
+
+void exInter1Handler1() interrupt 2 {
+	i = ++i % 16;
+	P2 = table[i];
 }
 
 int main() {
-	P0 = 0xFF;
-	P3 = 0xFF;
-	
-	TCON = 0x05;
-	IE = 0x85;
-	
-	while(1) {
-		P2 = led_mod[i];
-	}
+	initPorts();
+	initInterrupt();
+	while(1);
 }
-
