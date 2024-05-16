3a86a96bf650f035fff9ffc7b48b93d0ad86cb23
IAKSH <1248675212@qq.com>
2024-05-16 18:49:02 +0800

    luogu_p5788 6AC 4LTE

--- cpp_playground/lanqiaobei/luogu_p5788.cpp
+++ cpp_playground/lanqiaobei/luogu_p5788.cpp
@@ -1,35 +1,64 @@
 // https://www.luogu.com.cn/problem/P5788
 // 6AC 4LTE
+// 似乎内存操作太多了，并没有比之前O(n^2)更快
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// 先写一个无脑的O(n^2)
+struct Data {
+    int val;
+    int index;
+};
+
+auto comp = [](const Data& m,const Data& n) {
+    return m.val < n.val;
+};
 
 int main() {
-    std::ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 
     int n;cin >> n;
-    std::vector<int> v(n);
-    for(auto& i : v)
-        cin >> i;
+    stack<Data> s,s1;
+    priority_queue<Data,std::deque<Data>,decltype(comp)> pq(comp);
+    vector<int> res(n);
+    for(int i = 0;i < n;i++) {
+        Data d;
+        cin >> d.val;
+        d.index = i;
+        pq.emplace(d);
+    }
 
-    int i,j;
-    int len = v.size();
-    bool flag;
-    for(i = 0;i < len;i++) {
-        flag = true;
-        for(j = i + 1;j < len;j++) {
-            if(v[i] < v[j]) {
-                cout << j + 1 << ' ';
-                flag = false;
+    while(!pq.empty()) {
+        while(true) {
+            if(s.empty()) {
+                s.emplace(pq.top());
+                res[pq.top().index] = 0;
+                pq.pop();
+                while(!s1.empty()) {
+                    s.emplace(s1.top());
+                    s1.pop();
+                }
+                break;
+            }
+            else if(pq.top().index > s.top().index || pq.top().val > s.top().val) {
+                s1.emplace(s.top());
+                s.pop();
+            }
+            else {
+                res[pq.top().index] = s.top().index + 1;
+                s.emplace(pq.top());
+                pq.pop();
+                while(!s1.empty()) {
+                    s.emplace(s1.top());
+                    s1.pop();
+                }
                 break;
             }
         }
-        if(flag)
-            cout << "0 ";
     }
 
+    for(const auto& i : res)
+        cout << i << ' ';
     return 0;
 }
