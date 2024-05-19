5b37c274c9d1d77c56e6e5ad73af33f3b35dc93f
IAKSH <1248675212@qq.com>
2024-05-19 21:22:30 +0800

    luogu_p3865 1AC 2WA 10TLE

---
+++ cpp_playground/lanqiaobei/luogu_p3865.cpp
@@ -0,0 +1,29 @@
+// https://www.luogu.com.cn/problem/P3865
+// 先来个无脑的
+// 甚至捞了1个AC，2 WA，10 TLE
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int n,m,i,a,b;
+    cin >> n >> m;
+    int arr[n];
+    for(i = 0;i < n;i++)
+        cin >> arr[i];
+
+    //int res[m];
+
+    for(i = 0;i < m;i++) {
+        cin >> a >> b;
+        cout << *max_element(arr + a - 1,arr + b - 1) << '\n';
+        //res[i] = *max_element(arr + a - 1,arr + b - 1);
+    }
+
+    //for(const auto& i : res)
+    //    cout << i << '\n';
+    return 0;
+}
