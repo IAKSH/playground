cbe63310e63b7326a9776594dd5808cf99c8f42b
IAKSH <1248675212@qq.com>
2024-05-23 13:46:29 +0800

    trash

---
+++ cpp_playground/lanqiaobei/lqb_245.cpp
@@ -0,0 +1,16 @@
+// https://www.lanqiao.cn/problems/245/learning/?page=1&first_category_id=1&tags=%E5%9B%BD%E8%B5%9B,%E9%80%92%E6%8E%A8&sort=pass_rate&asc=0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    //int res = 0;
+    //for(int i = 0;i < n;i++) {
+//
+    //}
+
+    cout << pow(10,9) + 7;
+}
---
+++ cpp_playground/lanqiaobei/luogu_p3372.cpp
@@ -0,0 +1,9 @@
+// https://www.luogu.com.cn/problem/P3372
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+}
