9533525d2e38b115048c584bb481ace40753c3e8
IAKSH <1248675212@qq.com>
2024-05-23 14:01:45 +0800

    lqb_245

--- cpp_playground/lanqiaobei/lqb_245.cpp
+++ cpp_playground/lanqiaobei/lqb_245.cpp
@@ -1,4 +1,6 @@
 // https://www.lanqiao.cn/problems/245/learning/?page=1&first_category_id=1&tags=%E5%9B%BD%E8%B5%9B,%E9%80%92%E6%8E%A8&sort=pass_rate&asc=0
+// 同余，以及找规律（？
+// 100%
 
 #include <bits/stdc++.h>
 
@@ -7,10 +9,15 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
 
-    //int res = 0;
-    //for(int i = 0;i < n;i++) {
-//
-    //}
+    int n;
+    cin >> n;
 
-    cout << pow(10,9) + 7;
+    long long res = 0;
+    for(int i = 0;i < n;i++) {
+        res += static_cast<long long>(i) * (n - i) * (n - i);
+        res %= 1000000007;
+    }
+
+    cout << res << '\n';
+    return 0;
 }
