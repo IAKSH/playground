4827643b4a75f8e52dcb6f0da632474d1af3fdaf
IAKSH <1248675212@qq.com>
2024-05-24 21:58:13 +0800

    luogu_p8306

--- cpp_playground/lanqiaobei/luogu_p8306.cpp
+++ cpp_playground/lanqiaobei/luogu_p8306.cpp
@@ -9,140 +9,50 @@ using namespace std;
 // 字典树在查找上只比哈希表节约一些内存吧，除此之外啥也不是
 // 但是字典树能做前缀匹配，哈希表不能
 
-// 支持A-Z a-z 0-9的字典树
-// 因为是打比赛用的，所以没有delete
-class Trie {
-private:
-    struct TrieNode {
-        TrieNode* children[62];
-        bool isEndOfWord;
-        TrieNode() {
-            isEndOfWord = false;
-            for (int i = 0; i < 62; i++)
-                children[i] = nullptr;
+// 复制的fusu的题解
+// https://www.luogu.com.cn/article/uaqlf4xc
+struct Node {
+    int cnt;
+    std::unordered_map<char, Node*> ch;
+    
+    Node() : cnt(0) {};
+    
+    void dfs() {
+        for (auto [x, y] : ch) {
+            y->dfs();
+            cnt += y->cnt;
         }
-    };
-
-    int charToInt(char c) {
-        if(c >= 'A' && c <= 'Z')
-            return c - 'A';
-        if(c >= 'a' && c <= 'z')
-            return c - 'a' + 26;
-        if(c >= '0' && c <= '9')
-            return c - '0' + 52;
-        return -1;
-    }
-
-    int countWords(TrieNode* node) {
-        if (!node)
-            return 0;
-        int count = 0;
-        if (node->isEndOfWord)
-            count++;
-        for (int i = 0; i < 62; i++)
-            if (node->children[i])
-                count += countWords(node->children[i]);
-        return count;
-    }
-
-    char intToChar(int i) {
-        if(i >= 0 && i < 26)
-            return 'A' + i;
-        if(i >= 26 && i < 52)
-            return 'a' + (i - 26);
-        if(i >= 52 && i < 62)
-            return '0' + (i - 52);
-        return ' ';
-    }
-
-    void findAllWords(TrieNode* node, string &match, vector<string> &matches) {
-        if (node->isEndOfWord)
-            matches.push_back(match);
-        for (int i = 0; i < 62; i++) {
-            if (node->children[i]) {
-                match.push_back(intToChar(i));
-                findAllWords(node->children[i], match, matches);
-                match.pop_back();
-            }
-        }
-    }
-
-public:
-    TrieNode* root;
-
-    Trie() {
-        root = new TrieNode();
-    }
-
-    void insert(const string &word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            int index = charToInt(c);
-            if (!node->children[index])
-                node->children[index] = new TrieNode();
-            node = node->children[index];
-        }
-        node->isEndOfWord = true;
-    }
-
-    bool search(const string &word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            int index = charToInt(c);
-            if (!node->children[index])
-                return false;
-            node = node->children[index];
-        }
-        return node != nullptr && node->isEndOfWord;
-    }
-
-    int prefixCount(const string &prefix) {
-        TrieNode* node = root;
-        for (char c : prefix) {
-            int index = charToInt(c);
-            if (!node->children[index])
-                return 0;
-            node = node->children[index];
-        }
-        return countWords(node);
-    }
-
-    vector<string> prefixMatch(const string &prefix) {
-        vector<string> matches;
-        TrieNode* node = root;
-        string match;
-        for (char c : prefix) {
-            int index = charToInt(c);
-            if (!node->children[index])
-                return matches;
-            node = node->children[index];
-            match.push_back(c);
-        }
-        findAllWords(node, match, matches);
-        return matches;
     }
 };
 
+Node *rot;
 
 int main() {
-    ios::sync_with_stdio(false);
-
-    int t,n,q,l;
-
-    Trie trie;
-    string s;
-    cin >> t;
-    for(int i = 0;i < t;i++) {
-        cin >> n >> q;
-        for(int j = 0;j < n;j++) {
-            cin >> s;
-            trie.insert(s);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int T, n, q;
+    std::string s;
+    for (std::cin >> T; T; --T) {
+        std::cin >> n >> q;
+        rot = new Node();
+        for (int i = 1; i <= n; ++i) {
+            std::cin >> s;
+            auto u = rot;
+            for (auto c : s) u = (u->ch[c] ? u->ch[c] : u->ch[c] = new Node);
+            ++u->cnt;
         }
-        for(int k = 0;k < q;k++) {
-            cin >> s;
-            cout << trie.prefixCount(s) << '\n';
+        rot->dfs();
+        for (int i = 1; i <= q; ++i) {
+            std::cin >> s;
+            bool flag = true;
+            auto u = rot;
+            for (auto c : s) if (u->ch[c]) {
+                u = u->ch[c];
+            } else {
+                flag = false; break;
+            }
+            std::cout << ((flag) ? u->cnt : 0) << '\n';
         }
     }
-
     return 0;
 }
