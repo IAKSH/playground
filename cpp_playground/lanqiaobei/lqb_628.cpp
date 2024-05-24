dba311f670592597acf42bf10449128c7ff294f8
IAKSH <1248675212@qq.com>
2024-05-24 10:56:51 +0800

    lqb_628

---
+++ cpp_playground/lanqiaobei/lqb_628.cpp
@@ -0,0 +1,20 @@
+// https://www.lanqiao.cn/problems/628/learning/?page=1&first_category_id=1&second_category_id=3&tags=%E5%9B%BD%E8%B5%9B&sort=pass_rate&asc=0
+// 就是题目稍微有点绕
+// 小学题
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    //int j;
+    //for(int i = 20;i <= 100;i++) {
+    //    j = 2 * (i - 20);
+    //    if(j + 20 == 1.5 * i) {
+    //        cout << i << '\n';
+    //        break;
+    //    }
+    //}
+    cout << 40 << '\n';
+    return 0;
+}
