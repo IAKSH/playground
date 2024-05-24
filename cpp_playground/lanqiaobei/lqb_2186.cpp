6a30713f8b3a0f527dc4579667f8cfa4408d0d16
IAKSH <1248675212@qq.com>
2024-05-24 14:11:25 +0800

    lqb_2186

--- cpp_playground/lanqiaobei/lqb_2186.cpp
+++ cpp_playground/lanqiaobei/lqb_2186.cpp
@@ -1,29 +1,26 @@
 //https://www.lanqiao.cn/problems/2186/learning/?page=1&first_category_id=1&second_category_id=3&tags=%E5%9B%BD%E8%B5%9B,2022&sort=pass_rate&asc=0
-// 好像做成01背包了，而且没有去重
-// 暂存
+// 抄了别人的背包DP实现
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// 2022拆成10个不同的正整数之和，有多少种方法
+int main() noexcept {   
+    vector<vector<vector<long long>>> dp(2023,vector<vector<long long>>(11,vector<long long>(2023)));
 
-int main() noexcept {
-    int n = 2022;
-    
-    //array<array<int,2023>,2023> dp;
-    vector<vector<int>> dp(2023,vector<int>(2023));
-    for(int i = 0;i <= 2022;i++) {
-        for(int j = 0;j <= 2022;j++) {
-            // 需要在这里去重，或者在这个循环结束后
-            if(i >= j)
-                dp[i][j] = dp[i - j][j - 1] + 1;
-            else
-                dp[i][j] = dp[i][j - 1];
+    for(int i = 0;i <= 2022;i++)
+        dp[i][0][0] = 1;// 放不下，什么都不选也是一种选择
+
+    for(int i = 1;i <= 2022;i++) {
+        for(int j = 1;j <= 10;j++) {
+            for(int k = 1;k <= 2022;k++) {
+                dp[i][j][k] = dp[i - 1][j][k];
+                if(k >= i)
+                    dp[i][j][k] += dp[i - 1][j - 1][k - i];
+            }
         }
-        // 或者在这里去重
     }
 
-    cout << *max_element(dp[2022].begin(),dp[2022].end()) << '\n';
+    cout << dp[2022][10][2022] << '\n';
     return 0;
 }
