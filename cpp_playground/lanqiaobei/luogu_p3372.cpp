6dd4c6931366495abcd2015b988ae9cec1dc7929
IAKSH <1248675212@qq.com>
2024-05-24 20:44:58 +0800

    luogu_p3372

--- cpp_playground/lanqiaobei/luogu_p3372.cpp
+++ cpp_playground/lanqiaobei/luogu_p3372.cpp
@@ -1,9 +1,187 @@
 // https://www.luogu.com.cn/problem/P3372
+// 看起来树状数组只是线段树青春版
+// 树状数组本身只能实现点修改和区间求和
+// 树状数组+差分数组能实现单点查询和区间修改
+// 而线段树似乎可以在 O(log N) 的时间复杂度内实现:
+// 单点修改
+// 区间修改
+// 区间查询（区间求和，求区间最大值，求区间最小值
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+#ifdef A
+
+// 从 https://oi-wiki.org/ds/seg/#c-%E6%A8%A1%E6%9D%BF 复制来的模板
+template <typename T>
+class SegTreeLazyRangeAdd {
+  vector<T> tree, lazy;
+  vector<T> *arr;
+  int n, root, n4, end;
+
+  void maintain(int cl, int cr, int p) {
+    int cm = cl + (cr - cl) / 2;
+    if (cl != cr && lazy[p]) {
+      lazy[p * 2] += lazy[p];
+      lazy[p * 2 + 1] += lazy[p];
+      tree[p * 2] += lazy[p] * (cm - cl + 1);
+      tree[p * 2 + 1] += lazy[p] * (cr - cm);
+      lazy[p] = 0;
+    }
+  }
+
+  T range_sum(int l, int r, int cl, int cr, int p) {
+    if (l <= cl && cr <= r) return tree[p];
+    int m = cl + (cr - cl) / 2;
+    T sum = 0;
+    maintain(cl, cr, p);
+    if (l <= m) sum += range_sum(l, r, cl, m, p * 2);
+    if (r > m) sum += range_sum(l, r, m + 1, cr, p * 2 + 1);
+    return sum;
+  }
+
+  void range_add(int l, int r, T val, int cl, int cr, int p) {
+    if (l <= cl && cr <= r) {
+      lazy[p] += val;
+      tree[p] += (cr - cl + 1) * val;
+      return;
+    }
+    int m = cl + (cr - cl) / 2;
+    maintain(cl, cr, p);
+    if (l <= m) range_add(l, r, val, cl, m, p * 2);
+    if (r > m) range_add(l, r, val, m + 1, cr, p * 2 + 1);
+    tree[p] = tree[p * 2] + tree[p * 2 + 1];
+  }
+
+  void build(int s, int t, int p) {
+    if (s == t) {
+      tree[p] = (*arr)[s];
+      return;
+    }
+    int m = s + (t - s) / 2;
+    build(s, m, p * 2);
+    build(m + 1, t, p * 2 + 1);
+    tree[p] = tree[p * 2] + tree[p * 2 + 1];
+  }
+
+ public:
+  explicit SegTreeLazyRangeAdd<T>(vector<T> v) {
+    n = v.size();
+    n4 = n * 4;
+    tree = vector<T>(n4, 0);
+    lazy = vector<T>(n4, 0);
+    arr = &v;
+    end = n - 1;
+    root = 1;
+    build(0, end, 1);
+    arr = nullptr;
+  }
+
+  void show(int p, int depth = 0) {
+    if (p > n4 || tree[p] == 0) return;
+    show(p * 2, depth + 1);
+    for (int i = 0; i < depth; ++i) putchar('\t');
+    printf("%d:%d\n", tree[p], lazy[p]);
+    show(p * 2 + 1, depth + 1);
+  }
+
+  T range_sum(int l, int r) { return range_sum(l, r, 0, end, root); }
+
+  void range_add(int l, int r, int val) { range_add(l, r, val, 0, end, root); }
+};
+
 int main() {
+    ios::sync_with_stdio(false);
+
+    int n,m,i,j,x,y,k;
+    cin >> n >> m;
+
+    vector<int> v(n);
+    for(i = 0;i < n;i++)
+        cin >> v[i];
     
+    SegTreeLazyRangeAdd<int> s(v);
+
+    for(i = 0;i < m;i++) {
+        cin >> j;
+        if(j == 1) {
+            cin >> x >> y >> k;
+            s.range_add(x,y,k);
+        }
+        else {
+            cin >> x >> y;
+            cout << s.range_sum(x,y);
+        }
+    }
+
+    return 0;
 }
+
+#else
+
+// 笑死，全WA
+
+// 复制的oi-wiki题解
+
+typedef long long LL;
+LL n, a[100005], d[270000], b[270000];
+
+void build(LL l, LL r, LL p) {  // l:区间左端点 r:区间右端点 p:节点标号
+  if (l == r) {
+    d[p] = a[l];  // 将节点赋值
+    return;
+  }
+  LL m = l + ((r - l) >> 1);
+  build(l, m, p << 1), build(m + 1, r, (p << 1) | 1);  // 分别建立子树
+  d[p] = d[p << 1] + d[(p << 1) | 1];
+}
+
+void update(LL l, LL r, LL c, LL s, LL t, LL p) {
+  if (l <= s && t <= r) {
+    d[p] += (t - s + 1) * c, b[p] += c;  // 如果区间被包含了，直接得出答案
+    return;
+  }
+  LL m = s + ((t - s) >> 1);
+  if (b[p])
+    d[p << 1] += b[p] * (m - s + 1), d[(p << 1) | 1] += b[p] * (t - m),
+        b[p << 1] += b[p], b[(p << 1) | 1] += b[p];
+  b[p] = 0;
+  if (l <= m)
+    update(l, r, c, s, m, p << 1);  // 本行和下面的一行用来更新p*2和p*2+1的节点
+  if (r > m) update(l, r, c, m + 1, t, (p << 1) | 1);
+  d[p] = d[p << 1] + d[(p << 1) | 1];  // 计算该节点区间和
+}
+
+LL getsum(LL l, LL r, LL s, LL t, LL p) {
+  if (l <= s && t <= r) return d[p];
+  LL m = s + ((t - s) >> 1);
+  if (b[p])
+    d[p << 1] += b[p] * (m - s + 1), d[(p << 1) | 1] += b[p] * (t - m),
+        b[p << 1] += b[p], b[(p << 1) | 1] += b[p];
+  b[p] = 0;
+  LL sum = 0;
+  if (l <= m)
+    sum =
+        getsum(l, r, s, m, p << 1);  // 本行和下面的一行用来更新p*2和p*2+1的答案
+  if (r > m) sum += getsum(l, r, m + 1, t, (p << 1) | 1);
+  return sum;
+}
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  LL q, i1, i2, i3, i4;
+  std::cin >> n >> q;
+  for (LL i = 1; i <= n; i++) std::cin >> a[i];
+  build(1, n, 1);
+  while (q--) {
+    std::cin >> i1 >> i2 >> i3;
+    if (i1 == 2)
+      std::cout << getsum(i2, i3, 1, n, 1) << std::endl;  // 直接调用操作函数
+    else
+      std::cin >> i4, update(i2, i3, i4, 1, n, 1);
+  }
+  return 0;
+}
+
+#endif
