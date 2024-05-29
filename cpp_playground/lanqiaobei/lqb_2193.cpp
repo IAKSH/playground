0b5946643d90a386b963472f29d80000aff21931
IAKSH <1248675212@qq.com>
2024-05-29 21:37:51 +0800

    lqb_2193 50%

--- cpp_playground/lanqiaobei/lqb_2193.cpp
+++ cpp_playground/lanqiaobei/lqb_2193.cpp
@@ -1,11 +1,14 @@
 // https://www.lanqiao.cn/problems/2193/learning/?page=8&first_category_id=1&second_category_id=3&tags=%E5%9B%BD%E8%B5%9B&sort=pass_rate&asc=0
-// nmd，还是得用字符串
+// 实际上不用字符串（用字符串会非常低效）
+// 解决方案是在dfs之前给n扩一位来进行保护，只要记录之前的位数以及计算max的时候减回来就行了
+// 另外，似乎爆int了还是什么，等下修
+// 50%
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int n,a,b,m = 0,max_res = 0;
+int n,a,b,m = 0,max_res = 0,offset;
 
 int opA(int x,int ind) {
     int val = x;
@@ -22,36 +25,42 @@ int opB(int x,int ind) {
         val /= 10;
     val %= 10;
     int new_val = (val == 0 ? 9 : val - 1);
-    if(ind == m - 1 && new_val == 0)
-        x += pow(10,m);
     return x - (val - new_val) * pow(10,ind);
 }
 
 void dfs(int x,int ind,bool op,int cur_a,int cur_b) {
     if(op)
-        max_res = max(max_res,x = opA(x,ind));
+        max_res = max(max_res,(x = opA(x,ind)) - offset);
     else
-        max_res = max(max_res,x = opB(x,ind));
-    if(ind + 1 < m) {
+        max_res = max(max_res,(x = opB(x,ind)) - offset);
+    for(int i = 0;i < m;i++) {
         if(cur_a)
-            dfs(x,ind + 1,true,cur_a - 1,cur_b);
+            dfs(x,i,true,cur_a - 1,cur_b);
         if(cur_b)
-            dfs(x,ind + 1,false,cur_a,cur_b - 1);
+            dfs(x,i,false,cur_a,cur_b - 1);
     }
 }
+
+int countDigits(int x) {
+    int res = 0;
+    while(x > 0) {
+        x /= 10;
+        ++res;
+    }
+    return res;
+} 
 
 int main() {
     ios::sync_with_stdio(false);
     cin >> n >> a >> b;
+    n += (offset = pow(10,m = countDigits(n)));
 
-    int tmp = n;
-    while(tmp > 0) {
-        tmp /= 10;
-        ++m;
+    for(int i = 0;i < m;i++) {
+        if(a)
+            dfs(n,i,true,a - 1,b);
+        if(b)
+            dfs(n,i,false,a,b - 1);
     }
-
-    dfs(n,0,true,a,b);
-    dfs(n,0,false,a,b);
 
     cout << max_res << '\n';
     return 0;
