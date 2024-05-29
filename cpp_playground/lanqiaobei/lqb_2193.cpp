c18e9335d07055c4ee2d272c23d3256dbe4ee0f6
IAKSH <1248675212@qq.com>
2024-05-29 21:51:31 +0800

    lqb_2193

--- cpp_playground/lanqiaobei/lqb_2193.cpp
+++ cpp_playground/lanqiaobei/lqb_2193.cpp
@@ -1,34 +1,51 @@
 // https://www.lanqiao.cn/problems/2193/learning/?page=8&first_category_id=1&second_category_id=3&tags=%E5%9B%BD%E8%B5%9B&sort=pass_rate&asc=0
 // 实际上不用字符串（用字符串会非常低效）
 // 解决方案是在dfs之前给n扩一位来进行保护，只要记录之前的位数以及计算max的时候减回来就行了
-// 另外，似乎爆int了还是什么，等下修
+// 用了long long还是有几个爆了，而且快速幂都搬上来了还是有超时
+// 看来得剪枝
 // 50%
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int n,a,b,m = 0,max_res = 0,offset;
+int a,b,m;
+long long n,offset,max_res = 0;
 
-int opA(int x,int ind) {
+long long quick_power(long long a, long long b,long long c = LONG_LONG_MAX) {
+	long long m = a,n = b,res = 1;
+    while(n > 0) {
+        if(n & 1)// 更快的n % 2 == 1
+            res = res * m % c;
+        m = m * m % c;
+        n >>= 1;// 也许更快的n /= 2;
+    }
+    return res % c;
+}
+
+long long opA(long long x,int ind) {
     int val = x;
     for(int i = 0;i < ind;i++)
         val /= 10;
     val %= 10;
-    int new_val = (val == 9 ? 0 : val + 1);
-    return x - (val - new_val) * pow(10,ind);
+    if(val == 9)
+        return x - 9 * quick_power(10,ind);
+    else
+        return x + quick_power(10,ind);
 }
 
-int opB(int x,int ind) {
+long long opB(long long x,int ind) {
     int val = x;
     for(int i = 0;i < ind;i++)
         val /= 10;
     val %= 10;
-    int new_val = (val == 0 ? 9 : val - 1);
-    return x - (val - new_val) * pow(10,ind);
+    if(val == 0)
+        return x + 9 * quick_power(10,ind);
+    else
+        return x - quick_power(10,ind);
 }
 
-void dfs(int x,int ind,bool op,int cur_a,int cur_b) {
+void dfs(long long x,int ind,bool op,int cur_a,int cur_b) {
     if(op)
         max_res = max(max_res,(x = opA(x,ind)) - offset);
     else
@@ -53,7 +70,7 @@ int countDigits(int x) {
 int main() {
     ios::sync_with_stdio(false);
     cin >> n >> a >> b;
-    n += (offset = pow(10,m = countDigits(n)));
+    n += (offset = quick_power(10,m = countDigits(n)));
 
     for(int i = 0;i < m;i++) {
         if(a)
