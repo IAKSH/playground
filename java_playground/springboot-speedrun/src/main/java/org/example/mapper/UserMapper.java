8f65d518bfec8606740a656dd61eaed87c3a2006
IAKSH <1248675212@qq.com>
2024-06-14 22:39:56 +0800

    pure annotation mybatis

--- java_playground/springboot-speedrun/src/main/java/org/example/mapper/UserMapper.java
+++ java_playground/springboot-speedrun/src/main/java/org/example/mapper/UserMapper.java
@@ -1,12 +1,19 @@
 package org.example.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import org.example.entity.User;
 
 @Mapper
 public interface UserMapper {
+    @Select("SELECT * FROM users WHERE id = #{id}")
     User getUser(Long id);
+
+    @Insert("INSERT INTO users (name, age, email) VALUES (#{name}, #{age}, #{email})")
     int insertUser(User user);
+
+    @Update("UPDATE users SET name = #{name}, age=#{age}, email = #{email} WHERE id = #{id}")
     int updateUser(User user);
+
+    @Delete("DELETE FROM users WHERE id = #{id}")
     int deleteUser(Long id);
 }
