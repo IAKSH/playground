03baadc97de4672e46ed513750fc291e52dd0605
IAKSH <1248675212@qq.com>
2024-09-26 20:33:34 +0800

    leetcode find-first-and-last-position-of-element-in-sorted-array (faster)

--- cpp_playground/lanqiaobei/leetcode_find_first_and_last_position_of_element_in_sorted_array.cpp
+++ cpp_playground/lanqiaobei/leetcode_find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -4,19 +4,52 @@
 
 using namespace std;
 
+//#define USE_STD_BOUND
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
-        int r = upper_bound(nums.begin(),nums.end(),target) - nums.begin() - 1;
+        if(nums.size() == 0)
+            return {-1,-1};
+#ifdef USE_STD_BOUND
+        int r, l = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        //int r = upper_bound(nums.begin(),nums.end(),target) - nums.begin() - 1;
+        for(r = l;r + 1 < nums.size() && nums[r + 1] == target;r++);
         if(l > r)
             return {-1,-1};
         return {l,r};
+#else
+        int l = 0, r = nums.size() - 1, m = 0;
+        while(l < r) {
+            m = (l + r) / 2;
+            if(nums[m] == target) {
+                l = r = m;
+                break;
+            }
+            else if(nums[m] < target) {
+                l = m + 1;
+            }
+            else {
+                r = m;
+            }
+        }
+
+        for(;l - 1 >= 0 && nums[l - 1] == target;l--);
+        for(r = l;r + 1 < nums.size() && nums[r + 1] == target;r++);
+   
+        if(l == r) {
+            if(nums[l] == target)
+                return {l,l};
+            else
+                return {-1,-1};
+        }
+        return {l,r};
+#endif
     }
 };
 
 int main() {
-    vector<int> nums{1};
+    vector<int> nums{1,1,2};
     auto&& v = Solution().searchRange(nums,1);
     cout << '{' << v[0] << ',' << v[1] << "}\n";
     return 0;
