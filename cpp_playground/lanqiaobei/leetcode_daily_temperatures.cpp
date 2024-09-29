2df10f7abbe5b5c2fcb77eeb318f3b66a0466f67
IAKSH <1248675212@qq.com>
2024-09-29 19:34:38 +0800

    leetcode daily-temperatures

---
+++ cpp_playground/lanqiaobei/leetcode_daily_temperatures.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.cn/problems/daily-temperatures/description/?envType=study-plan-v2&envId=top-100-liked
+// 单调栈
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> s;
+        vector<int> result(temperatures.size(),0);
+        for(int i = 0;i < temperatures.size();i++) {
+            while(!s.empty()) {
+                if(temperatures[i] <= s.top().first)
+                    break;
+                result[s.top().second] = i - s.top().second;
+                s.pop();
+            }
+            s.emplace(make_pair(temperatures[i],i));
+        }
+        return result;
+    }
+};
+
+int main() {
+    vector<int> temps{73,74,75,71,69,72,76,73};
+    cout << '{';
+    for(const auto& i : Solution().dailyTemperatures(temps))
+        cout << i << ',';
+    cout << "\b}\n";
+}
+
+/*
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出: [1,1,4,2,1,1,0,0]
+*/
