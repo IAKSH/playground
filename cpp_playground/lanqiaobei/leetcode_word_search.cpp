24504bc1a64ab612bc9190a1f6172ec250662c9a
IAKSH <1248675212@qq.com>
2024-10-11 15:44:17 +0800

    leetcode word-search faster

--- cpp_playground/lanqiaobei/leetcode_word_search.cpp
+++ cpp_playground/lanqiaobei/leetcode_word_search.cpp
@@ -7,26 +7,26 @@ using namespace std;
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> mark(board.size(),vector<bool>(board[0].size()));
         for(int i = 0;i < board.size();i++) {
             for(int j = 0;j < board[0].size();j++)
-                if(dfs(board,mark,word,i,j,0))
+                if(dfs(board,word,i,j,0))
                     return true;
         }
         return false;
     }
 
 private:
-    bool dfs(const vector<vector<char>>& board,vector<vector<bool>>& mark,const string& word,int i,int j,int cnt) {
-        if(!mark[i][j] && cnt < word.size() && board[i][j] == word[cnt]) {
-            mark[i][j] = true;
+    bool dfs(vector<vector<char>>& board,const string& word,int i,int j,int cnt) {
+        if(cnt < word.size() && board[i][j] != '\0' && board[i][j] == word[cnt]) {
+            char c = board[i][j];
+            board[i][j] = '\0';
             if(cnt == word.size() - 1 ||
-                ((i > 0 && dfs(board,mark,word,i - 1,j,cnt + 1)) ||
-                (i < board.size() - 1 && dfs(board,mark,word,i + 1,j,cnt + 1)) ||
-                (j > 0 && dfs(board,mark,word,i,j - 1,cnt + 1)) ||
-                (j < board[0].size() - 1 && dfs(board,mark,word,i,j + 1,cnt + 1))))
+                ((i > 0 && dfs(board,word,i - 1,j,cnt + 1)) ||
+                (i < board.size() - 1 && dfs(board,word,i + 1,j,cnt + 1)) ||
+                (j > 0 && dfs(board,word,i,j - 1,cnt + 1)) ||
+                (j < board[0].size() - 1 && dfs(board,word,i,j + 1,cnt + 1))))
                 return true;
-            mark[i][j] = false;
+            board[i][j] = c;
         }
         return false;
     }
@@ -34,12 +34,10 @@ private:
 
 int main() {
     vector<vector<char>> board{
-        //{'A','B','C','E'},
-        //{'S','F','C','S'},
-        //{'A','D','E','E'}
-        {'A','B'}
+        {'A','B','C','E'},
+        {'S','F','C','S'},
+        {'A','D','E','E'}
     };
-    //cout << (Solution().exist(board,"ABCCED") ? "true" : "false") << '\n';
-    cout << (Solution().exist(board,"BA") ? "true" : "false") << '\n';
+    cout << (Solution().exist(board,"ABCCED") ? "true" : "false") << '\n';
     return 0;
 }
