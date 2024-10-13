a39fa419bbd4e2784d84dd99312ca30a25508fc6
IAKSH <1248675212@qq.com>
2024-10-13 10:58:51 +0800

    leetcode palindrome-partitioning but slow

---
+++ cpp_playground/lanqiaobei/leetcode_palindrome_partitioning.cpp
@@ -0,0 +1,61 @@
+// https://leetcode.cn/problems/palindrome-partitioning/description/?envType=study-plan-v2&envId=top-100-liked
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> results;
+        dfs(s,results,vector<string>(),0,s.size());
+        return results;
+    }
+
+private:
+    void dfs(const string& s,vector<vector<string>>& results,vector<string> v,int l,int len) {
+        v.emplace_back(s.substr(l,len));
+        if(l + len == s.size()) {
+            bool b = true;
+            for(const auto& i : v) {
+                if(!check(i)) {
+                    b = false;
+                    break;
+                }
+            }
+            if(b)
+                results.emplace_back(v);
+        }
+        for(int i = 1;i < s.size() - l;i++) {
+            auto new_v = v;
+            new_v.pop_back();
+            new_v.emplace_back(s.substr(l,i));
+            dfs(s,results,new_v,l + i,s.size() - l - i);
+        }
+    }
+
+    bool check(const string& s) {
+        if(s.size() == 1)
+            return true;
+        int l = 0,r = s.size() - 1;
+        while(l < r) {
+            if(s[l] != s[r])
+                return false;
+            ++l;
+            --r;
+        }
+        return true;
+    }
+};
+
+int main() {
+    for(const auto& v : Solution().partition("aab")) {
+        cout << '{';
+        for(const auto& c : v) {
+            cout << c << ',';
+        }
+        cout << "\b},";
+    }
+    cout << "\b \n";
+    return 0;
+}
