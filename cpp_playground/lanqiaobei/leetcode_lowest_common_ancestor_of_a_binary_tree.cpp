8f841beb17a09199a75a1938e67a58e56c838b5d
IAKSH <1248675212@qq.com>
2024-11-04 19:23:46 +0800

    leetcode lowest-common-ancestor-of-a-binary-tree

---
+++ cpp_playground/lanqiaobei/leetcode_lowest_common_ancestor_of_a_binary_tree.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        result = nullptr;
+        val1 = p->val;
+        val2 = q->val;
+        dfs(root);
+        return result;
+    }
+
+private:
+    bool dfs(TreeNode* node) {
+        if(result)
+            return false;
+
+        bool b1,b2;
+        b1 = b2 = false;
+        if(node->left)
+            b1 = dfs(node->left);
+        if(node->right)
+            b2 = dfs(node->right);
+    
+        if(node->val == val1 || node->val == val2) {
+            if(b1 || b2)
+                result = node;
+            return true;
+        }
+        else {
+            if(b1 && b2)
+                result = node;
+            return (b1 || b2);
+        }
+    }
+
+    int val1,val2;
+    TreeNode* result;
+};
+
+int main() {
+    cout << "no test for you!\n";
+    return 0;
+}
