57a147ac6f5062ad5b0153378185f7227926a3f2
IAKSH <1248675212@qq.com>
2024-11-12 13:33:20 +0800

    leetcode decode-string using stringstream

--- cpp_playground/lanqiaobei/leetcode_decode_string.cpp
+++ cpp_playground/lanqiaobei/leetcode_decode_string.cpp
@@ -8,6 +8,7 @@ class Solution {
 public:
     string decodeString(string s) {
         stack<int> st;
+        stringstream ss;
         for(int i = 0;i < s.size();i++) {
             if(s[i] == '[')
                 st.emplace(i);
@@ -17,15 +18,14 @@ public:
                 for(j = st.top() - 1;j >= 0 && s[j] >= '0' && s[j] <= '9';j--)
                     n += pow(10,st.top() - 1 - j) * (s[j] - '0');
 
-                string l(s.substr(0,j + 1));
-                string r(s.substr(i + 1));
-                string m(s.substr(st.top() + 1,i - st.top() - 1));
-
+                ss << s.substr(0,j + 1);
                 for(int k = 0;k < n;k++)
-                    l += m;
-                l += r;
+                    ss << s.substr(st.top() + 1,i - st.top() - 1);
+                ss << s.substr(i + 1);
+                s = ss.str();
+                ss.str("");
+
                 i = st.top() - 1;
-                s = l;
                 st.pop();
             }
         }
@@ -37,23 +37,3 @@ int main() {
     cout << Solution().decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef") << '\n';
     return 0;
 }
-
-/*
-输入：s = "3[a]2[bc]"
-输出："aaabcbc"
-
-输入：s = "3[a2[c]]"
-输出："accaccacc"
-
-输入：s = "abc3[cd]xyz"
-输出："abccdcdcdxyz"
-
-"3[z]2[2[y]pq4[2[jk]e1[f]]]ef"
-zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef
-zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef
-zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef
-zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef
-
-"100[leetcode]"
-
-*/
