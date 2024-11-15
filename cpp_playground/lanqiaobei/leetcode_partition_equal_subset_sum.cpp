d11d5689ee591b8f91dd5dfa3c79a7b30642bb6d
IAKSH <1248675212@qq.com>
2024-11-15 17:20:06 +0800

    leetcode partition-equal-subset-sum TLE

--- cpp_playground/lanqiaobei/leetcode_partition_equal_subset_sum.cpp
+++ cpp_playground/lanqiaobei/leetcode_partition_equal_subset_sum.cpp
@@ -4,24 +4,42 @@
 
 using namespace std;
 
-/**
- *
-*/
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        if(sum % 2 == 1) {
+        half = accumulate(nums.begin(),nums.end(),0);
+        half /= 2.0f;
+        if(floorf(half) != half)
             return false;
+        vector<bool> mask(nums.size(),false);
+        for(int i = 0;i < nums.size();i++) {
+            if(dfs(nums,mask,0,0))
+                return true;
         }
-
-        
+        return false;
     }
+
+private:
+    bool dfs(vector<int>& nums,vector<bool>& mask,int acc,int index) {
+        mask[index] = true;
+        acc += nums[index];
+        if(acc == half)
+            return true;
+        for(int i = 0;i < mask.size();i++) {
+            if(!mask[i]) {
+                if(dfs(nums,mask,acc,i))
+                    return true;
+             }
+        }
+        mask[index] = false;
+        return false;
+    }
+
+    float half = 0.0f;
 };
 
 int main() noexcept {
-    vector<int> v{1,5,11,5};
-    Solution s;
-    cout << s.canPartition(v) << endl;
+    vector<int> v{14,9,8,4,3,2};
+    cout << Solution().canPartition(v) << endl;
     return 0;
 }
