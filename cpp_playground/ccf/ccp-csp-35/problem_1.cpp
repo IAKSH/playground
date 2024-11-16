21cc518e566c013d8d4192966c5e6b8c066659cf
IAKSH <1248675212@qq.com>
2024-11-16 16:01:38 +0800

    fix problem_1

--- cpp_playground/ccf/ccp-csp-35/problem_1.cpp
+++ cpp_playground/ccf/ccp-csp-35/problem_1.cpp
@@ -1,59 +1,38 @@
-// https://sim.csp.thusaac.com/contest/35/problem/1
-// 16AC 4TLE
-
 #include <bits/stdc++.h>
+// 16AC 4TLE
 
 using namespace std;
 
 int main() {
-    string s,temp_s,input;
+    string s,trans_s,tmp;
     getline(cin,s);
     s.erase(s.begin());
     s.erase(s.end() - 1);
 
-    unordered_map<char,char> trans,tmp_map_1,tmp_map_2;
+    unordered_map<char,char> trans;
     stringstream ss;
 
     int n,m,k;
-    getline(cin,input);
-    ss << input;
+    getline(cin,tmp);
+    ss << tmp;
     ss >> n;
 
     for(int i = 0;i < n;i++) {
-        getline(cin,input);
-        trans[input[1]] = input[2];
+        getline(cin,tmp);
+        trans[tmp[1]] = tmp[2];
     }
 
     cin >> m;
     for(int i = 0;i < m;i++) {
         cin >> k;
-        temp_s = s;
-        tmp_map_1 = trans;
-        tmp_map_2 = trans;
-
-        for(int j = 0;j < k - 1;j++) {
-            for(auto& [k,v] : tmp_map_2) {
-                char mk = v;
-                bool b = true;
-                while(tmp_map_1.count(mk) != 0) {
-                    mk = tmp_map_1[mk];
-                    if(mk == k) {
-                        tmp_map_2.erase(k);
-                        b = false;
-                        break;
-                    }
-                }
-                if(b)
-                    v = mk;
+        trans_s = s;
+        for(int j = 0;j < k;j++) {
+            for(auto& c : trans_s) {
+                if(trans.count(c) > 0)
+                    c = trans[c];
             }
-            tmp_map_1 = tmp_map_2;
         }
-
-        for(auto& c : temp_s) {
-            if(tmp_map_1.count(c) > 0)
-                c = tmp_map_1[c];
-        }
-        cout << '#' << temp_s << "#\n";
+        cout << '#' << trans_s << "#\n";
     }
 
     return 0;
