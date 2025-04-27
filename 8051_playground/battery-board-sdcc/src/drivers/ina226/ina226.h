fb79e31635bfa702488d0842a82212aff0d4fc2c
IAKSH <1248675212@qq.com>
2025-04-27 21:17:47 +0800

    moved

--- 8051_playground/battery-board-sdcc/src/drivers/ina226/ina226.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INA226_H__
-#define __INA226_H__
-
-void ina226_init(void);
-float ina226_read_volt(void);
-float ina226_read_current(void);
-float ina226_read_power(void);
-
-#endif
--- 8051_playground/battery-board/src/adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ADC_H__
-#define __ADC_H__
-
-void adc_init();
-unsigned short adc_convert();
-float adc_to_voltage(unsigned short adc_value);
-
-#endif
