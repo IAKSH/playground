8652ea9539fc5bf205486276895e74128574a0b7
IAKSH <1248675212@qq.com>
2025-05-21 22:59:35 +0800

    roll back to stm32-for-vscode

--- embedded/f4-balance-bike/erase.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Erase the target device
-OPENOCD=/usr/bin/openocd
-
-"$OPENOCD" -f ./openocd.cfg -c "init; reset halt; stm32f4x mass_erase 0; exit"
--- embedded/f4-balance-bike/flash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Flash the program to the target device
-OPENOCD=/usr/bin/openocd
-RELEASE_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/build
-TARGET=BalanceBike
-
-"$OPENOCD" -f ./openocd.cfg -c "program $RELEASE_DIRECTORY/$TARGET.elf verify reset exit"
